1e65125cca2bd1953e2415ea4bad6329af15af61
trueanqu <bogdan.oblapenko@gmail.com>
2019-11-05 11:46:55 +0200

    add article controller, remove non-primitive  type from article model

--- MVCArticles/Models/Article.cs
+++ MVCArticles/Models/Article.cs
@@ -11,7 +11,6 @@ namespace MVCArticles.Models
         public string Name { get; set; }
         public string Author { get; set; }
         public string Content { get; set; }
-        public List<string> Tags { get; set; }
 
     }
 }
